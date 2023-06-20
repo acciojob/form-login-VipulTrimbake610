cfec7a74e4ac632b49fbfa04528d78bac2c013ca
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-20 11:15:44 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.getElementsByTagName("input")[0];
+	const lname = document.getElementsByTagName("input")[1];
+
+	alert(fname,lname);
 
 }
